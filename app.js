cabfb7c0724eee468c8efb3fc19d7da37f77771c
Mbwebwa Lukusa <16ml250@esisalama.org>
2020-01-25 17:35:21 +0200

    lancement du serveur au port 3000

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
+app.set('view engine', 'pug');
+
 app.listen(port, () => {
     console.log(`Serveur lancé sur le port ${port}`);
 });
