39b380e23fa3a6485108f694d3f6bded03be930a
Mbwebwa Lukusa <16ml250@esisalama.org>
2020-01-27 05:50:23 +0200

    Etablir la connexion a la base de donnnees local repertoire

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(methodOverride('_method'));
 
 app.set('view engine', 'pug');
 
@@ -8,6 +12,7 @@ const indexRoute = require("./routes/index");
 
 app.use("/", indexRoute);
 
+const port = 3000;
 app.listen(port, () => {
     console.log(`Serveur lancé sur le port ${port}`);
 });
